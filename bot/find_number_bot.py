83dfbee7eb56c03c960c2cc6a0c5387d959c4ed8
Nerdniwe <moontan.ln@gmail.com>
2026-03-10 13:56:14 +0400

    added find_number_bot

---
+++ bot/find_number_bot.py
@@ -0,0 +1,33 @@
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message
+from aiogram.filters import BaseFilter
+
+BOT_TOKEN = '8344205366:AAHDWFxMopRjqqqidWXdGMW3M2wXAMS05mI'
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+class NumberInMessage(BaseFilter):
+    async def __call__(self, message: Message) -> bool | dict[str, list[int]]:
+        numbers = []
+        for word in message.text.split():
+            normalized_word = word.replace('.', '').replace(',', '').strip()
+            if normalized_word.isdigit():
+                numbers.append(int(normalized_word))
+        if numbers:
+            return {'numbers': numbers}
+        return False
+
+@dp.message(F.text.lower().startswith('найди числа'), NumberInMessage())
+async def process_if_numbers(message: Message, numbers: list[int]):
+    await message.answer(text = f'Нашел: {", ".join(str(num) for num in numbers)}')
+
+@dp.message(F.text.lower().startswith('найди числа'))
+async def process_if_not_numbers(message: Message):
+    await message.answer(text = 'Чисел не нашлось!')
+
+@dp.message()
+async def process_if_not_find(message: Message):
+    await message.answer(text = 'Не понимаю :(')
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
