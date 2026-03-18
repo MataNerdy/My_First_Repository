e84195d7dc1268406f32cf67fca480128b0ea4ac
Nerdniwe <moontan.ln@gmail.com>
2026-03-18 22:33:00 +0400

    imports and __init__

--- Packs/main.py
+++ Packs/main.py
@@ -1,3 +1,6 @@
-from pack_2.pack_21.file_211 import r
 print("Это основной модуль main.py, его имя в процессе выполнения:", __name__)
-print("r =", r)
+
+import pack_1
+
+print(dir())
+print(dir(pack_1))
--- Packs/pack_1/file_11.py
+++ Packs/pack_1/file_11.py
@@ -1,8 +1,16 @@
-from .file_12 import num
+__all__ = ['some_var', 'yet_some_var', 'another_func']
 
 print("Это модуль", __name__)
 
-def some_func(n: int) -> float:
-    return (n + n) / n**n
+some_var: int  = 42
+another_var: int = 242
+yet_some_var: str = 'some_text'
 
-result = some_func(num)
+def _reverse_text(text: str) -> str:
+    return text[::-1]
+
+def some_func(text: str, times: int) -> str:
+    return _reverse_text(text * times)
+
+def another_func() -> None:
+    pass
