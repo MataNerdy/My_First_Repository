95f8e429f9c104c6b89ff1bc577ebbd4a485b0de
Nerdniwe <moontan.ln@gmail.com>
2026-03-21 21:59:42 +0400

    results of logistic regression

--- AI_Education/EDA_students/eda_students_ml.py
+++ AI_Education/EDA_students/eda_students_ml.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
 df = pd.read_csv('/Users/Di/Documents/GitHub/My projects/My_First_Repository/AI_Education/EDA_students/eda_students_final.csv')
 
@@ -12,10 +13,12 @@ X['test preparation course'] = X['test preparation course'].map({"completed" : 1
 X = X.drop(['parental level of education', 'class_group', 'gender'], axis=1)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-print(X_train.shape, X_test.shape)
 
 model = LogisticRegression()
 model.fit(X_train, y_train)
 pred = model.predict(X_test)
 
-print(y_test.value_counts(normalize=True))
+print(f"\n{y_test.value_counts(normalize=True)}")
+print(f"\nWrong predictions: {sum(abs(y_test-pred))}\n")
+print(f"\nConfusion_matrix:\n{confusion_matrix(y_test, pred)}\n")
+print(f"\nWeights:{model.coef_}")
