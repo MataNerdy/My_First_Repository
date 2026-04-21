1372cdf10bc464ff24100e7b47c73337779dfb7a
Nerdniwe <moontan.ln@gmail.com>
2026-04-22 02:13:50 +0400

    yaml

--- TG_bot/logging_config_example/main.py
+++ TG_bot/logging_config_example/main.py
@@ -1,6 +1,9 @@
 import logging.config
-from logging_settings import logging_config
+import yaml
 from module_1 import main
 
-logging.config.dictConfig(logging_config)
+with open('logging_config.yaml', 'rt') as f:
+    config = yaml.safe_load(f.read())
+
+logging.config.dictConfig(config)
 main()
