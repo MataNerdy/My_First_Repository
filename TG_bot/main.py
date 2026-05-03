14a4884541087b2e3f8c5884583cc2513bedc857
Nerdniwe <moontan.ln@gmail.com>
2026-05-03 21:40:59 +0400

    env

--- TG_bot/main.py
+++ TG_bot/main.py
@@ -1,6 +1,14 @@
 import os
-import dotenv
+from environs import Env
 
-dotenv.load_dotenv()
+env = Env()
+env.read_env(override=True)
+bot_token = env("BOT_TOKEN")
+admin_id = env("ADMIN_ID")
+
+print(f"Bot token: {bot_token}")
+print(f"Admin ID: {admin_id}")
+print()
+
 print(os.getenv("BOT_TOKEN"))
 print(os.getenv("ADMIN_ID"))
