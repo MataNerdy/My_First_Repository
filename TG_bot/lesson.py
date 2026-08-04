847f5324eb7e08be118712385684d98de73fef4e
Nerdniwe <moontan.ln@gmail.com>
2026-08-04 23:05:50 +0400

    crypto tg bot

---
+++ TG_bot/lesson.py
@@ -0,0 +1,41 @@
+
+from dotenv import load_dotenv
+import os
+from telebot import TeleBot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+import requests
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+bot = TeleBot(TOKEN)
+CRYPTO_NAME_TO_TICKER = {
+    "Bitcoin": "BTCUSDT",
+    "Ethereum": "ETHUSDT",
+    "Doge": "DOGEUSDT"
+}
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = ReplyKeyboardMarkup(row_width=3)
+    for crypto_name in CRYPTO_NAME_TO_TICKER:
+        item_button = KeyboardButton(crypto_name)
+        markup.add(item_button)
+    bot.send_message(message.chat.id, "Choose a cryptocurrency:", reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text in CRYPTO_NAME_TO_TICKER)
+def send_price(message):
+    crypto_name = message.text
+    ticker = CRYPTO_NAME_TO_TICKER[crypto_name]
+    price = get_price_by_ticker(ticker=ticker)
+    bot.send_message(message.chat.id, f"The current price of {crypto_name} is {price}")
+
+def get_price_by_ticker(*, ticker: str) -> float:
+    endpoint = "https://api.binance.com/api/v3/ticker/price"
+    params = {"symbol": ticker}
+    response = requests.get(endpoint, params=params)
+    data = response.json()
+    price = round(float(data["price"]), 2)
+    return price
+
+bot.infinity_polling()
